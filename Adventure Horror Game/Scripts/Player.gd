42e741ab94fa088e6e58e2116faf705feeae171c
Diyllan <dr.manican@gmail.com>
2023-03-28 18:45:10 +0200

    new Pixelart

--- Adventure Horror Game/Scripts/Player.gd
+++ Adventure Horror Game/Scripts/Player.gd
@@ -6,6 +6,7 @@ var target = Vector2.ZERO
 var interactable_object
 var in_inventory: bool
 var dialogueActive: bool
+var pickingUP:bool
 @export var interacting = false
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
@@ -13,6 +14,7 @@ var dialogueActive: bool
 
 func _ready():
 	pass
+	$AnimationPlayer.play("Idle")
 
 func _physics_process(_delta):
 	
@@ -36,10 +38,12 @@ func _physics_process(_delta):
 		var dir = navigation_agent.get_next_path_position() - position
 		velocity = dir.normalized() * speed
 		move_and_slide()
+		$AnimationPlayer.play("Walking")
 		
-	else:
+	elif !pickingUP:
 		velocity = Vector2.ZERO
 		navigation_agent.set_debug_enabled(false)
+		$AnimationPlayer.play("Idle")
 		if interactable_object == null:
 			return
 		if position.distance_to(interactable_object.position) < 50 and interacting:
@@ -54,7 +58,7 @@ func _physics_process(_delta):
 func execute(interactable_object):
 	match interactable_object.Interact_Type:
 		"print_text" : print(interactable_object.Interact_Value)
-		"pickup" : PlayerInventory.add_item(interactable_object)
+		"pickup" : PlayerInventory.add_item(interactable_object) and $AnimationPlayer.play("PickingUp")
 		"use" : PlayerInventory.use_Item(interactable_object)
 		"DialogueBox" : DialogueManager.showDialogueBox(interactable_object)
 		"DialogueBubble" : DialogueManager.showDialogueBubble(interactable_object.Examine_Text)
